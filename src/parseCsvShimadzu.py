0d0c1ac45b22d84be8ad3c6884d8b2c6d0910628
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2017-09-14 19:46:15 -0300

    Fix #2

--- src/parseCsvShimadzu.py
+++ src/parseCsvShimadzu.py
@@ -1,7 +1,9 @@
+import re
+
 def parseCsvShimadzu(file_content):
-    data = {
-            "sample": "AFR390", 
-            "current": 1000, 
-            "livetime": 959
-           }
+    data = {}
+    data['sample'] = file_content.split(',')[0].split(':')[1].replace("\"", "").strip()
+    data['current'] = re.sub(' +',' ',file_content.split(',')[12]).split(' ')[3]
+    data['current'] = int(re.findall('\d+', data['current'])[0])
+    data['livetime'] = int(re.sub(' +',' ',file_content.split(',')[12]).split(' ')[13])
     return(data)
