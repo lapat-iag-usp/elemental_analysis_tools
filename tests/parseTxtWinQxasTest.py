5e7e565352e5035830199b8d9a24b6683485ee22
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-02-02 18:08:27 -0200

    new things

--- tests/parseTxtWinQxasTest.py
+++ tests/parseTxtWinQxasTest.py
@@ -25,8 +25,8 @@ class Test_parseTxtWinQxas(unittest.TestCase):
         self.assertEqual(parseTxtWinQxas(file_content)['K']['errors'][13],133)
 
     def test_14(self):
-        self.assertEqual(parseTxtWinQxas(file_content)['K']['peaks'][14],38268+171)
-        self.assertEqual(parseTxtWinQxas(file_content)['K']['errors'][14],math.sqrt(282**2+298**2))
+        self.assertEqual(parseTxtWinQxas(file_content)['K']['peaks'][14],38268)
+        self.assertEqual(parseTxtWinQxas(file_content)['K']['errors'][14],282)
 
     def test_22(self):
         self.assertEqual(parseTxtWinQxas(file_content)['K']['peaks'][22],19355)
@@ -37,10 +37,10 @@ class Test_parseTxtWinQxas(unittest.TestCase):
         self.assertEqual(parseTxtWinQxas(file_content)['L']['errors'][56],214)
 
     def test_42(self):
-        self.assertEqual(parseTxtWinQxas(file2_content)['K']['peaks'][42],372924+2236)
-        self.assertEqual(parseTxtWinQxas(file2_content)['L']['peaks'][42],30490+5882)
-        self.assertEqual(parseTxtWinQxas(file2_content)['K']['errors'][42],math.sqrt(625**2+115**2))
-        self.assertEqual(parseTxtWinQxas(file2_content)['L']['errors'][42],math.sqrt(293**2+389**2))
+        self.assertEqual(parseTxtWinQxas(file2_content)['K']['peaks'][42],372924)
+        self.assertEqual(parseTxtWinQxas(file2_content)['L']['peaks'][42],30490)
+        self.assertEqual(parseTxtWinQxas(file2_content)['K']['errors'][42],625)
+        self.assertEqual(parseTxtWinQxas(file2_content)['L']['errors'][42],293)
 
 if __name__ == '__main__':
     unittest.main()
