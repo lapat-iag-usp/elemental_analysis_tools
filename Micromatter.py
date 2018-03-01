74a1a395406068f0aeb835c1ea85c1f420011f8c
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-03-01 13:16:12 -0300

    Micrommater as class

--- Micromatter.py
+++ Micromatter.py
@@ -1,40 +1,42 @@
 import csv 
 
-def csv2list():
-    micromatter_IAGUSP = []
-    with open('app/services/tests/data/calibration/micromatter_IAGUSP.csv') as linhas:
-        linhascsv = csv.reader(linhas,delimiter=',')
-        for linha in linhascsv:
-            micromatter_IAGUSP.append(linha)
+def Micromatter():
 
-    # remove cabeçalho
-    micromatter_IAGUSP.pop(0)
-    return micromatter_IAGUSP
+    def csv2list():
+        micromatter_IAGUSP = []
+        with open('app/services/tests/data/calibration/micromatter_IAGUSP.csv') as linhas:
+            linhascsv = csv.reader(linhas,delimiter=',')
+            for linha in linhascsv:
+                micromatter_IAGUSP.append(linha)
+
+        # remove cabeçalho
+        micromatter_IAGUSP.pop(0)
+        return micromatter_IAGUSP
 
 
-def serials():
-    micromatter_IAGUSP = []
-    with open('app/scripts/tests/data/calibration/micromatter_IAGUSP.csv') as linhas:
-        linhascsv = csv.reader(linhas,delimiter=',')
-        for linha in linhascsv:
-            micromatter_IAGUSP.append(linha[0])
+    def serials():
+        micromatter_IAGUSP = []
+        with open('app/scripts/tests/data/calibration/micromatter_IAGUSP.csv') as linhas:
+            linhascsv = csv.reader(linhas,delimiter=',')
+            for linha in linhascsv:
+                micromatter_IAGUSP.append(linha[0])
 
-    # remove cabeçalho
-    micromatter_IAGUSP.pop(0)
-    return micromatter_IAGUSP
+        # remove cabeçalho
+        micromatter_IAGUSP.pop(0)
+        return micromatter_IAGUSP
 
-def serials4flask():
-    retorno = []
-    lista = serials()
-    for i in lista:
-        tupla = (i,i)
-        retorno.append(tupla)
-    return retorno
+    def serials4flask():
+        retorno = []
+        lista = serials()
+        for i in lista:
+            tupla = (i,i)
+            retorno.append(tupla)
+        return retorno
 
-def getSample(id):
-    with open('app/scripts/tests/data/calibration/micromatter_IAGUSP.csv') as linhas:
-        linhascsv = csv.reader(linhas,delimiter=',')
-        for linha in linhascsv:
-            if linha[0] == id:
-                return(linha)
+    def getSample(id):
+        with open('app/scripts/tests/data/calibration/micromatter_IAGUSP.csv') as linhas:
+            linhascsv = csv.reader(linhas,delimiter=',')
+            for linha in linhascsv:
+                if linha[0] == id:
+                    return(linha)
 
