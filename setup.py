0d36002468329702338d76cc58e1a5450cc79d26
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-02-28 23:49:28 -0300

    amend

--- setup.py
+++ setup.py
@@ -4,27 +4,11 @@ setup(
     version="0.1",
     packages=find_packages(),
 
-    install_requires=['pyxray>=1.3'],
+    install_requires=['pyxray>=1.3','numpy>=1.14.1'],
 
-    package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
-        # And include any *.msg files found in the 'hello' package, too:
-        'hello': ['*.msg'],
-    },
-
-    # metadata for upload to PyPI
-    author="Me",
-    author_email="me@example.com",
-    description="This is an Example Package",
+    author="Thiago Gomes Veríssimo",
+    author_email="verissimotgv@gmail.com",
+    description="EAS",
     license="PSF",
-    keywords="hello world example examples",
-    url="http://example.com/HelloWorld/",   # project home page, if any
-    project_urls={
-        "Bug Tracker": "https://bugs.example.com/HelloWorld/",
-        "Documentation": "https://docs.example.com/HelloWorld/",
-        "Source Code": "https://code.example.com/HelloWorld/",
-    }
-
-    # could also include long_description, download_url, classifiers, etc.
+    keywords="x-rays"
 )
