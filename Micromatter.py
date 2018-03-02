bc95207b38ca5b867192978ba292a5ccdcf20768
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-03-02 20:04:20 -0300

    Test

--- Micromatter.py
+++ Micromatter.py
@@ -4,8 +4,8 @@ def csv2list():
     micromatter_IAGUSP = []
     with open('app/services/tests/data/calibration/micromatter_IAGUSP.csv') as linhas:
         linhascsv = csv.reader(linhas,delimiter=',')
-    for linha in linhascsv:
-        micromatter_IAGUSP.append(linha)
+        for linha in linhascsv:
+            micromatter_IAGUSP.append(linha)
 
     # remove cabeçalho
     micromatter_IAGUSP.pop(0)
@@ -16,14 +16,14 @@ def serials():
     micromatter_IAGUSP = []
     with open('app/scripts/tests/data/calibration/micromatter_IAGUSP.csv') as linhas:
         linhascsv = csv.reader(linhas,delimiter=',')
-    for linha in linhascsv:
-        micromatter_IAGUSP.append(linha[0])
+        for linha in linhascsv:
+            micromatter_IAGUSP.append(linha[0])
 
     # remove cabeçalho
     micromatter_IAGUSP.pop(0)
     return micromatter_IAGUSP
 
-def serials4flask(self):
+def serials4flask():
     retorno = []
     lista = serials()
     for i in lista:
@@ -34,9 +34,9 @@ def serials4flask(self):
 def getSample(id):
     with open('app/scripts/tests/data/calibration/micromatter_IAGUSP.csv') as linhas:
         linhascsv = csv.reader(linhas,delimiter=',')
-    for linha in linhascsv:
-        if linha[0] == id:
-    return(linha)
+        for linha in linhascsv:
+            if linha[0] == id:
+                return(linha)
   
 def bla():
     return "bal bla"
