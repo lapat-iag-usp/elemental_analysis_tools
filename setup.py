9c048efe17df822484148668e9173a309f3dd08b
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-03-02 19:47:55 -0300

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
+
 setup(
     name="eas",
     version="0.1",
-    packages=find_packages(),
-
-    install_requires=['pyxray>=1.3','numpy>=1.14.1'],
-
+    url = 'git@github.com:thiagogomesverissimo/eas.git',
     author="Thiago Gomes Veríssimo",
     author_email="verissimotgv@gmail.com",
-    description="EAS",
-    license="PSF",
-    keywords="x-rays"
+    description="EAS - elemental-analysis-scripts",
+    packages = find_packages(),    
+    install_requires=['pyxray>=1.3','numpy>=1.14.1'],
 )
