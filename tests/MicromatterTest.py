4627f947feaec075c28f39a0ee28b922bf011444
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-06-13 19:30:37 -0300

    fix path

--- tests/MicromatterTest.py
+++ tests/MicromatterTest.py
@@ -13,9 +13,9 @@ file_content = pathlib.Path(file_path).read_text()
 class MicromatterTest(unittest.TestCase):
 
     def test_34662(self):
-        Na = Micromatter.getMasses(file_content,34662)[11]
-        Cl = Micromatter.getMasses(file_content,34662)[17]
-        total = Micromatter.getMasses(file_content,34662)['total']
+        Na = Micromatter.getMasses(34662,file_content)[11]
+        Cl = Micromatter.getMasses(34662,file_content)[17]
+        total = Micromatter.getMasses(34662,file_content)['total']
 
         self.assertEqual(Na,19.5899428492)
         self.assertEqual(Cl,30.2100571508)
