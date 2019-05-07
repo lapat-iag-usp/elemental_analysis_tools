33ca872b958b0c14fcc5e47a3e01658e8c07e658
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2019-05-06 23:35:09 -0300

    docs removed

--- lib/Shimadzu.py
+++ lib/Shimadzu.py
@@ -1,8 +1,27 @@
 import re
 
 def parseCsv(file_content):
-    """
-            doc
+    """Example function with types documented in the docstring.
+
+    `PEP 484`_ type annotations are supported. If attribute, parameter, and
+    return types are annotated according to `PEP 484`_, they do not need to be
+    included in the docstring:
+
+    Parameters
+    ----------
+    param1 : int
+        The first parameter.
+    param2 : str
+        The second parameter.
+
+    Returns
+    -------
+    bool
+        True if successful, False otherwise.
+
+    .. _PEP 484:
+        https://www.python.org/dev/peps/pep-0484/
+
     """
     irradiation_parameters = {}
     irradiation_parameters['sample'] = file_content.split(',')[0].split(':')[1].replace("\"", "").strip()
