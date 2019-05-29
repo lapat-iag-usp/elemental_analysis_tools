6c278aee05eb215328887651277e2b6c250675ee
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2019-05-29 17:41:16 -0300

    response factor

--- lib/responseFactor.py
+++ lib/responseFactor.py
@@ -2,10 +2,10 @@
 calculateResponseFactor: N/(density*current*livetime)
 """
 
-def ResponseFactor(N,density,current,livetime):
+def responseFactor(N,density,current,livetime):
 
     return(N/(density*current*livetime))
 
-def ResponseFactorError(N,density,current,livetime):
+def responseFactorError(N,density,current,livetime):
 
     return('Not implemented yet')
