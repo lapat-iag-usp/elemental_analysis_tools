c7f4a7752bbea6451ae47a3c14c03e01af7aa5e6
Vmirwald <vitor.mirwald@usp.br>
2019-06-05 14:44:09 -0300

    Function to do Error estimate of response factor Added

--- lib/responseFactor.py
+++ lib/responseFactor.py
@@ -1,11 +1,24 @@
 """
 calculateResponseFactor: N/(density*current*livetime)
 """
+import numpy as np
 
 def responseFactor(N,density,current,livetime):
-
-    return(N/(density*current*livetime))
+    
+    R = N/(density*current*livetime)
+    return R
 
 def responseFactorError(N,density,current,livetime):
-
-    return('Not implemented yet')
+    '''This function do the calc of Error'''
+    
+    #iniciando e atribuindo variáveis
+    N = N
+    Dens = density
+    Current = current
+    time = livetime
+    #Calculando o fator de resposta
+    R = responseFactor(N,Dens,Current,time)
+    #Propagando incerteza
+    sigR = R*np.sqrt((sigN/N)**2+(sigDens/Dens)**2+(sigCurrent/Current)**2+(sigTime/Time)**2) #propagação de incerteza
+    
+    return sigR #Retorna a incerteza do fator de resposta
