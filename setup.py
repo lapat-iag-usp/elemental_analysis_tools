9854b194e02734b3127ec6a976034c6dddd241d2
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-05-18 00:43:56 -0300

    renamed

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="elemental-analysis-scripts",
-    version="1.0.0",
+    name="elemental_analysis_scripts",
+    version="0.0.1",
     url = 'https://github.com/elemental-analysis-group/elemental-analysis-scripts.git',
+    description="elemental-analysis-scripts package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Thiago Gomes Veríssimo",
     author_email="verissimotgv@gmail.com",
-    description="elemental-analysis-scripts",
     packages=setuptools.find_packages(),
     install_requires=['pyxray>=1.3','numpy>=1.14.1'],
 )
